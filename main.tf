76dbe92f9693ada1e368dc4d6aa0b73a41c2eeb1
solivalle <127651078+solivalle@users.noreply.github.com>
2026-06-11 20:33:19 -0600

    Task 2 - Create the CI runner role with OIDC trust

--- main.tf
+++ main.tf
@@ -28,3 +28,49 @@ resource "aws_iam_openid_connect_provider" "github" {
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 
 }
+
+output "oidc_provider_arn" {
+  value = aws_iam_openid_connect_provider.github.arn
+}
+
+resource "aws_iam_role" "ci_runner" {
+  name = "8-2-ci-runner-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.github.arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          "token.actions.githubusercontent.com:sub" = "repo:${aws_iam_openid_connect_provider.github_repo}:ref:refs/heads/main"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "ci_runner" {
+  name = "8-2-ci-runner-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid      = "CallerIdentity"
+        Effect   = "Allow"
+        Action   = ["sts:GetCallerIdentity"]
+        Resource = ["*"]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ci_runner" {
+  role       = aws_iam_role.ci_runner.name
+  policy_arn = aws_iam_policy.ci_runner.arn
+}
